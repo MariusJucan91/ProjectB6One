70350464c3e0ff972c854c8b9f543b24d330a025
jucan <jucan@DESKTOP-2FU5P1H.lan>
2021-12-22 19:18:38 -0500

    Test HomeWork...

---
+++ src/two/DemoTwo.java
@@ -0,0 +1,9 @@
+package two;
+
+public class DemoTwo {
+	public static void main(String[] args) {
+		System.out.println("Initial Code...");
+		//test how to add your home work on github.com
+		
+	}
+}
